0a83b1b21569393f50331f03b2202fe604314b4e
zhexuand <64929452+zhexuand@users.noreply.github.com>
2020-08-29 11:45:59 -0700

    Static test updated.

--- statichomepage/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StatichomepageConfig(AppConfig):
-    name = 'statichomepage'
--- statichomepage/views.py
+++
@@ -1,4 +0,0 @@
-from django.views.generic import TemplateView
-
-class HelloWorld(TemplateView):
-    template_name = 'Hellotest.html'
