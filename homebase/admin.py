9cc247f024b0e5a6967dda828af2152e55029011
zhexuand <64929452+zhexuand@users.noreply.github.com>
2020-08-31 03:01:37 -0700

    1.basic css/js added. database updated (add comments/like/author fuction.)

--- homebase/admin.py
+++ homebase/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from homebase.models import ImagePost
+from homebase.models import ImagePost, WinescipeUser, Like, Comment
 
 # Register your models here.
 
 admin.site.register(ImagePost)
+admin.site.register(WinescipeUser)
+admin.site.register(Like)
+admin.site.register(Comment)
+
+
--- homebase/models.py
+++ homebase/models.py
@@ -4,7 +4,18 @@ from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class WinescipeUser(AbstractUser):
+    profile_image = ProcessedImageField(
+        upload_to = 'static/images/profile',
+        format = 'JPEG',
+        options = {'quality':100},
+        blank = True,
+        null = True
+        )
+        
+        
 class ImagePost(models.Model):
+    author = models.ForeignKey(WinescipeUser,on_delete=models.CASCADE,related_name='my_posts')
     title = models.TextField(blank = True,null = True)
     image = ProcessedImageField(
         upload_to = 'static/images/posts',
@@ -15,13 +26,20 @@ class ImagePost(models.Model):
         )
     def get_absolute_url(self):
         return reverse("postdetail",args=[str(self.id)])
-
-class WinescipeUser(AbstractUser):
-    profile_image = ProcessedImageField(
-        upload_to = 'static/images/profile',
-        format = 'JPEG',
-        options = {'quality':100},
-        blank = True,
-        null = True
-        )
+    
+class Like(models.Model):
+    post = models.ForeignKey(ImagePost,on_delete=models.CASCADE,related_name='likes')
+    user = models.ForeignKey(WinescipeUser,on_delete=models.CASCADE,related_name='likes')
+    class Meta:
+        unique_together = ("post", "user")
+    def __str__(self):
+        return self.user.username + ' likes ' + self.post.title
+        
+class Comment(models.Model):
+    post = models.ForeignKey(ImagePost,on_delete=models.CASCADE,related_name='comments')
+    user = models.ForeignKey(WinescipeUser,on_delete=models.CASCADE)
+    comment = models.CharField(max_length=150)
+    posted_on = models.DateTimeField(auto_now_add=True,editable=False)
+    def __str__(self):
+        return self.user.username + ' add a comment to Post:  ' + self.post.title
     
